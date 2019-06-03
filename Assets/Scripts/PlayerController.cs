7f380600bff75f0b462550684dec249f3fbceee3
LEAFERx <leaferx@outlook.com>
2019-06-03 13:39:05 +0800

    update

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,26 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
-  // [TODO] [IMPORTANT] Ground check
-
   // [TODO] Member too chaotic
-  private Rigidbody2D player;
+  private Rigidbody2D playerRb;
+  private Vector2 playerSize;
   private SpriteRenderer playerSprite;
-  private Transform groundDetector;
 
   private bool jumpRequest;
   private bool isGrounded;
   private float moveDirection;
   private float originXPosition;
   private Vector3 smoothVelocity = Vector3.zero;
+  private Vector2 groundDetectSize;
+
+  // Ground Detect Parameters
+  public Transform groundDetector;
+  public LayerMask groundLayer;
+  [Range(0.01f, 0.05f)] public float groundDetectThickness;
 
   // Horizontal Move Parameters
   [Range(1f, 10f)] public float moveVelocity = 3.0f;
   [Range(1f, 10f)] public float returnVelocity = 1.0f;
-  [Range(0f, 1f)] public float smoothTime = .05f;
+  [Range(0f, 1f)] public float smoothTime = 0.05f;
   [Range(1f, 10f)] public float leftBoundary = 2.0f;
   [Range(1f, 10f)] public float rightBoundary = 2.0f;
-  public float returnThreshold = 0.02f;
+  [Range(0.01f, 0.05f)] public float returnThreshold = 0.02f;
 
   // Jump Parameters
   [Range(1f, 50f)] public float jumpForce = 10f;
@@ -31,9 +35,11 @@ public class PlayerController : MonoBehaviour {
   [Range(1f, 10f)] public float lowjumpGravityScale = 2.0f;
 
   private void Start() {
-    player = GetComponent<Rigidbody2D>();
+    playerRb = GetComponent<Rigidbody2D>();
+    playerSize = GetComponent<BoxCollider2D>().size;
     playerSprite = GetComponent<SpriteRenderer>();
-    originXPosition = player.position.x;
+    originXPosition = playerRb.position.x;
+    groundDetectSize = new Vector2(playerSize.x * 0.9f, groundDetectThickness);
   }
 
   private void Flip() {
@@ -58,37 +64,41 @@ public class PlayerController : MonoBehaviour {
       Unflip();
     }
 
-    if (Input.GetButtonDown("Jump")) {
+    if (Input.GetButtonDown("Jump") && isGrounded) {
       jumpRequest = true;
+      isGrounded = false;
     }
   }
 
   private void FixedUpdate() {
+    // Ground detect
+    isGrounded = Physics2D.OverlapBox(groundDetector.position, groundDetectSize, 0f, groundLayer);
+
     MoveUpdate();
     JumpUpdate();
   }
 
   private void SmoothMove(float velocity) {
-    Vector3 targetVelocity = new Vector2(velocity, player.velocity.y);
-    player.velocity = Vector3.SmoothDamp(player.velocity, targetVelocity, ref smoothVelocity, smoothTime);
+    Vector3 targetVelocity = new Vector2(velocity, playerRb.velocity.y);
+    playerRb.velocity = Vector3.SmoothDamp(playerRb.velocity, targetVelocity, ref smoothVelocity, smoothTime);
   }
 
   private void MoveUpdate() {
-    // [TODO ]Need optimization
-    // 1 Press inverse key in boundary will stop moving
+    // [TODO] Need optimization
+    // 1 Press inverse key in boundary will stop moving ##
     // 2 Rest time before return?
-    // 3 Sometims shaking in origin
+    // 3 Sometimes shaking in origin (rarely)
     if (moveDirection != 0) {
-      if ((originXPosition - player.position.x < leftBoundary) && (player.position.x - originXPosition < rightBoundary)) {
+      if ((originXPosition - playerRb.position.x < leftBoundary) && (playerRb.position.x - originXPosition < rightBoundary)) {
         SmoothMove(moveDirection * moveVelocity);
       } else {
-        player.velocity = new Vector2(0f, player.velocity.y);
+        playerRb.velocity = new Vector2(0f, playerRb.velocity.y);
       }
     } else {
-      if (Mathf.Abs(originXPosition - player.position.x) > returnThreshold) {
-        SmoothMove(Mathf.Sign(originXPosition - player.position.x) * returnVelocity);
+      if (Mathf.Abs(originXPosition - playerRb.position.x) > returnThreshold) {
+        SmoothMove(Mathf.Sign(originXPosition - playerRb.position.x) * returnVelocity);
       } else {
-        player.velocity = new Vector2(0f, player.velocity.y);
+        playerRb.velocity = new Vector2(0f, playerRb.velocity.y);
       }
     }
   }
@@ -96,18 +106,18 @@ public class PlayerController : MonoBehaviour {
   private void JumpUpdate() {
     // Jump Request Handle
     if (jumpRequest) {
-      player.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+      playerRb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
 
       jumpRequest = false;
     }
 
     // Jump Optimize
-    if (player.velocity.y < 0) {
-      player.gravityScale = baseGravity * fallGravityScale;
-    } else if (player.velocity.y > 0 && !Input.GetButton("Jump")) {
-      player.gravityScale = baseGravity * lowjumpGravityScale;
+    if (playerRb.velocity.y < 0) {
+      playerRb.gravityScale = baseGravity * fallGravityScale;
+    } else if (playerRb.velocity.y > 0 && !Input.GetButton("Jump")) {
+      playerRb.gravityScale = baseGravity * lowjumpGravityScale;
     } else {
-      player.gravityScale = baseGravity;
+      playerRb.gravityScale = baseGravity;
     }
   }
 }
